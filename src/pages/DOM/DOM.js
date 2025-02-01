799f575ed50275fe1f8b5b9ecda4079b197dae33
Tuba-Younas17 <tubayounas1@gmail.com>
2025-02-01 04:31:42 -0800

    DOM manipualtion and working with objects

--- src/pages/DOM/DOM.js
+++ src/pages/DOM/DOM.js
@@ -54,6 +54,74 @@ console.log(showrroom);
 
 
 //DOM manipulation
+const container = document.querySelector("#courses-container"); 
+
+const coursedata = {
+    course1: {
+        name: "Web Development",
+        duration: 12,
+        courses: [
+            { name: "HTML", 
+                 duration: 3 },
+            { name: "CSS",
+                 duration: 2 },
+            { name: "JavaScript",
+                 duration: 4 },
+        ]
+    },
+    course2: {
+        name: "Mobile Development",
+        duration: 15,
+        courses: [
+            { name: "Android",
+                 duration: 6 },
+            { name: "iOS", 
+                duration: 7 },
+            { name: "Flutter",
+                 duration: 5 },
+        ]
+    }
+};
+
+
+for (let key in coursedata) {
+    const course = coursedata[key];
+
+    
+    const courseDiv = document.createElement("div");
+    courseDiv.className = "mini-project p-4 border bg-white shadow-md rounded-md mt-4";
+    
+    
+    const courseTitle = document.createElement("h1");
+    courseTitle.textContent = course.name;
+
+    
+    courseDiv.appendChild(courseTitle);
+
+  
+    container.appendChild(courseDiv);
+
+    let isExpanded = false;
+    courseDiv.addEventListener("click", function () {
+        if (isExpanded) {
+           
+            courseDiv.innerHTML = `<h1>${course.name}</h1>`;
+        } else {
+           
+            courseDiv.innerHTML = `<h1>${course.name}</h1><p>Course Duration: ${course.duration} months</p>`;
+            course.courses.forEach(element => {
+                const newElement = document.createElement("p");
+                newElement.textContent = element.name + " - " + element.duration + " months";
+                courseDiv.appendChild(newElement);
+            });
+        }
+        isExpanded = !isExpanded; 
+    });
+}
+
+
+
+//box
 const box1=document.querySelector("#box1");
 
 box1.addEventListener("mouseover", function(){
@@ -63,13 +131,13 @@ box1.addEventListener("mouseover", function(){
 })
 box1.addEventListener("mouseout", function(){
         box1.style.backgroundColor="";
-        box1.innerText="BOX 1";
+        box1.innerText="";
     })
 
 const tag=document.getElementById("appendElement");
 const newTag=document.createElement("p");
 newTag.innerText="This is a DOM Manipulation page";
-tag.append(newTag);
+tag.appendChild(newTag);
 console.log(newTag);
 const newTag1=document.createElement("p");
 newTag1.innerText="This is a new paragraph";
