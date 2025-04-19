a1ed95cd25940740417e61ebadde7d91b21b78c1
Salman Irfan <salmanirfan674@gmail.com>
2025-04-19 08:21:43 -0700

    get created user details in read api response

--- Backend/controllers/v1Controllers/adminController/getGroceryItemsController.js
+++ Backend/controllers/v1Controllers/adminController/getGroceryItemsController.js
@@ -1,13 +1,14 @@
 import { GroceryItem } from "../../../models/GroceryModelItem.js";
 
- export const getGroceryItemsController = async (req, res) => {
-			try {
-				const groceries = await GroceryItem.find(); // Fetch all grocery items
-				res.json(groceries);
-			} catch (error) {
-				console.error(" Error fetching grocery items:", error);
-				res.json({
-					error: "Failed to fetch grocery items",
-				});
-			}
-		};
+export const getGroceryItemsController = async (req, res) => {
+	try {
+		// const groceries = await GroceryItem.find().populate("createdBy", "name email"); // fetch specific user fields
+		const groceries = await GroceryItem.find().populate("createdBy", "-password");
+		res.json(groceries);
+	} catch (error) {
+		console.error("Error fetching grocery items:", error);
+		res.status(500).json({
+			error: "Failed to fetch grocery items",
+		});
+	}
+};
